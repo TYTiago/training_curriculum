3a05b118ec3fa6f2c0df5ec1ce1b761f89cea242
TYTiago <69578313+TYTiago@users.noreply.github.com>
2020-09-10 20:00:46 +0900

    Update calendars_controller.rb

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,7 +8,8 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-    Plan.create(plan_params)
+    binding.pry
+    @plan = Plan.new(plan_params)
     redirect_to action: :index
   end
 
